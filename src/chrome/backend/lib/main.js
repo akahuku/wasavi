ed6e1ac81ae5b78d4f36b9d65e0ff4a739cfe6ae
akahuku <akahuku@gmail.com>
2015-03-01 08:22:14 +0900

    update copyright year

--- src/chrome/backend/lib/main.js
+++ src/chrome/backend/lib/main.js
@@ -4,7 +4,7 @@
  * @author akahuku@gmail.com
  */
 /**
- * Copyright 2012-2014 akahuku, akahuku@gmail.com
+ * Copyright 2012-2015 akahuku, akahuku@gmail.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
