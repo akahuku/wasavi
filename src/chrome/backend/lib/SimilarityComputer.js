66d8f588532bac9cfcaf148da81ce7e291e678bd
akahuku <akahuku@gmail.com>
2017-01-19 23:04:03 +0900

    update copyright year

--- src/chrome/backend/lib/SimilarityComputer.js
+++ src/chrome/backend/lib/SimilarityComputer.js
@@ -4,7 +4,7 @@
  * @author akahuku@gmail.com
  */
 /**
- * Copyright 2012-2016 akahuku, akahuku@gmail.com
+ * Copyright 2012-2017 akahuku, akahuku@gmail.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
